1a43e3d493efdc09aaf2adaf4fa97b7b353e66ff
YurimYang <yang980329@likelion.org>
2025-01-13 17:05:38 +0900

    feat: mongoDB 시간 자동 수집 추가

--- src/main/java/com/example/board_project/global/config/MongoDBConfig.java
+++ src/main/java/com/example/board_project/global/config/MongoDBConfig.java
@@ -3,12 +3,14 @@ package com.example.board_project.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+@EnableMongoAuditing
 @Configuration
 public class MongoDBConfig {
     @Bean
