1a2e3bcc28fc3fc5a573a8a19f17473a954affaa
YurimYang <yang980329@likelion.org>
2025-01-13 14:57:26 +0900

    feat: mongoDB 연동 및 자동증가 구현

--- src/main/java/com/example/board_project/domain/entity/DatabaseSequence.java
+++ src/main/java/com/example/board_project/domain/entity/DatabaseSequence.java
@@ -1,12 +1,12 @@
 package com.example.board_project.domain.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.stereotype.Service;
 
 @Getter
-@Service
+@Setter
 @Document(collection = "database_sequences")
 /**
  * AUTO INCREMENT 연속성 정보를 저장하는 컬렉션
--- src/main/java/com/example/board_project/domain/events/BoardModelListener.java
+++ src/main/java/com/example/board_project/domain/events/BoardModelListener.java
@@ -5,8 +5,10 @@ import com.example.board_project.domain.service.SequenceGeneratorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Component
 public class BoardModelListener extends AbstractMongoEventListener<Board> {
     private final SequenceGeneratorService sequenceGeneratorService;
 
