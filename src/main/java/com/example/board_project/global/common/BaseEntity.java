70808caa68d68eadf428ab51a4d107a77105acff
YurimYang <yang980329@likelion.org>
2025-01-14 10:22:17 +0900

    feat: post 관련 시간 패턴 도입

--- src/main/java/com/example/board_project/global/common/BaseEntity.java
+++ src/main/java/com/example/board_project/global/common/BaseEntity.java
@@ -1,20 +1,27 @@
 package com.example.board_project.global.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class BaseEntity {
     @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
+
     @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt;
 
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime deletedAt;
 
     protected void delete(LocalDateTime currentTime) {
