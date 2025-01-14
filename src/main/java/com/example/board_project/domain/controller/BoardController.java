85670254e71dedc81da5f61b7e9abd9ce98f9ab0
YurimYang <yang980329@likelion.org>
2025-01-14 17:27:00 +0900

    fix: apiResponse 로 구조 통일

--- src/main/java/com/example/board_project/domain/controller/BoardController.java
+++ src/main/java/com/example/board_project/domain/controller/BoardController.java
@@ -6,6 +6,10 @@ import com.example.board_project.domain.dto.response.PostResponse;
 import com.example.board_project.domain.service.BoardService;
 import com.example.board_project.global.common.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -32,14 +36,18 @@ public class BoardController {
     }
 
     @GetMapping("/post-history")
-    @ResponseStatus(HttpStatus.OK)
+    @ApiResponse(responseCode = "200", description = "ok", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = PostResponse.class))
+    })
     @Operation(summary = "게시글 전체 조회 API", description = "게시판의 글을 전체 조회하는 API입니다.")
     public ResponseDTO<List<PostResponse>> getAllPosts() {
         return ResponseDTO.res(boardService.getAllPosts(), "게시판 전체 조회에 성공했습니다.");
     }
 
     @GetMapping("/post-history/{post_id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ApiResponse(responseCode = "200", description = "ok", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = PostResponse.class))
+    })
     @Operation(summary = "특정 게시글 조회 API", description = "게시판의 특정 글을 조회하는 API입니다.")
     public ResponseDTO<PostResponse> getPost(@PathVariable("post_id") String postId) {
         return ResponseDTO.res(boardService.getPost(postId), postId + "번 글 조회에 성공했습니다.");
