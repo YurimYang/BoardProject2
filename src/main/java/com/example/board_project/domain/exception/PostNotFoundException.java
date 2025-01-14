a352d30b3522bae3879d9ef569055b3713123fa6
YurimYang <yang980329@likelion.org>
2025-01-14 10:21:00 +0900

    feat: post 조회 불가능 예외처리

---
+++ src/main/java/com/example/board_project/domain/exception/PostNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.board_project.domain.exception;
+
+import com.example.board_project.global.exception.ApplicationException;
+import com.example.board_project.global.exception.ErrorCode;
+
+public class PostNotFoundException extends ApplicationException {
+    public PostNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
--- src/main/java/com/example/board_project/global/exception/ErrorCode.java
+++ src/main/java/com/example/board_project/global/exception/ErrorCode.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
+
     private final HttpStatus httpStatus;
     private final String simpleMessage;
 
