e0ef11439bf1b996ded67444949eb659f134f690
YurimYang <yang980329@likelion.org>
2025-01-14 15:16:39 +0900

    feat: Pagination 조회 설계

--- src/main/java/com/example/board_project/domain/repository/BoardRepository.java
+++ src/main/java/com/example/board_project/domain/repository/BoardRepository.java
@@ -1,7 +1,11 @@
 package com.example.board_project.domain.repository;
 
 import com.example.board_project.domain.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BoardRepository extends MongoRepository<Board, String> {
+
+    Page<Board> findAll(Pageable pageable);
 }
