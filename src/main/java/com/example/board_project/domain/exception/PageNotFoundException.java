1677cd710e6131f934c8ed635b36f435d5e798b1
YurimYang <yang980329@likelion.org>
2025-01-14 15:16:19 +0900

    feat: Pagination 조회 예외처리

---
+++ src/main/java/com/example/board_project/domain/exception/PageNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.board_project.domain.exception;
+
+import com.example.board_project.global.exception.ApplicationException;
+import com.example.board_project.global.exception.ErrorCode;
+
+public class PageNotFoundException extends ApplicationException {
+    public PageNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
--- src/main/java/com/example/board_project/global/exception/ErrorCode.java
+++ src/main/java/com/example/board_project/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "더 이상 페이지가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String simpleMessage;
