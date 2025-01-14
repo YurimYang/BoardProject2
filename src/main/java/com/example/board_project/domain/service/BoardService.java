a34e1fb266735513b40b4d3cd212572d1edd9040
YurimYang <yang980329@likelion.org>
2025-01-14 10:19:28 +0900

    feat: 특정 post 조회 서비스 로직 구현

--- src/main/java/com/example/board_project/domain/service/BoardService.java
+++ src/main/java/com/example/board_project/domain/service/BoardService.java
@@ -1,9 +1,12 @@
 package com.example.board_project.domain.service;
 
 import com.example.board_project.domain.dto.request.PostRequest;
-import com.example.board_project.domain.dto.response.AllPostsResponse;
+import com.example.board_project.domain.dto.response.PostResponse;
+import com.example.board_project.domain.entity.Board;
+import com.example.board_project.domain.exception.PostNotFoundException;
 import com.example.board_project.domain.repository.BoardRepository;
 import com.example.board_project.domain.util.BoardMapper;
+import com.example.board_project.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +25,13 @@ public class BoardService {
     }
 
     @Transactional(readOnly = true)
-    public List<AllPostsResponse> getAllPosts(){
+    public List<PostResponse> getAllPosts(){
         return BoardMapper.toAllPostListResponse(boardRepository.findAll());
     }
 
+    @Transactional(readOnly = true)
+    public PostResponse getPost(Long postId) {
+        Board searchedBoard = boardRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
+        return BoardMapper.toPostResponse(searchedBoard);
+    }
 }
