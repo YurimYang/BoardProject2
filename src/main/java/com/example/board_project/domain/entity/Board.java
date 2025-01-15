b796aa078f635750b46e092cd39ef00a01541bbb
YurimYang <yang980329@likelion.org>
2025-01-15 11:26:02 +0900

    feat: post seq 필드 추가

--- src/main/java/com/example/board_project/domain/entity/Board.java
+++ src/main/java/com/example/board_project/domain/entity/Board.java
@@ -1,11 +1,9 @@
 package com.example.board_project.domain.entity;
 
 import com.example.board_project.global.common.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
@@ -15,24 +13,23 @@ import java.util.UUID;
 @Document(collection = "board")
 public class Board extends BaseEntity {
 
-//    @Transient
-//    public static final String SEQUENCE_NAME = "board_sequence";
-//    //자동 증가 시퀀스에 대한 참조를 하는 SEQUENCE_NAME을 static 필드로 선언하고, @Transient를 사용해서 영속 필드에서 제외
+    @Transient
+    public static final String SEQUENCE_NAME = "board_sequence";
+    //자동 증가 시퀀스에 대한 참조를 하는 SEQUENCE_NAME을 static 필드로 선언하고, @Transient를 사용해서 영속 필드에서 제외
 
     @Id
     private String id;
+    @Setter
+    private Long seq;
     private String title;
     private String content;
     private String writer;
 
 
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-
     @Builder
-    public Board(String title, String content, String writer) {
+    public Board(Long seq, String title, String content, String writer) {
         this.id = UUID.randomUUID().toString();
+        this.seq = seq;
         this.title = title;
         this.content = content;
         this.writer = writer;
