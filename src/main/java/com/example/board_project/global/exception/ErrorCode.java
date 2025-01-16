b3b84eaf1c1405d42ec25a60bc76a7efe4624581
YurimYang <yang980329@likelion.org>
2025-01-16 11:25:52 +0900

    refactor: 불필요한 수식어 제거

--- src/main/java/com/example/board_project/global/exception/ErrorCode.java
+++ src/main/java/com/example/board_project/global/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
-    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "더 이상 페이지가 존재하지 않습니다.");
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "페이지가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String simpleMessage;
