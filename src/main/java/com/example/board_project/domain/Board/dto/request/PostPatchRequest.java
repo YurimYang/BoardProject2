706c188c884bfc0d21675fbf2b98ebd45df190d6
YurimYang <yang980329@likelion.org>
2025-01-20 08:49:19 +0900

    fix: test를 위한 builder 어노테이션 추가

--- src/main/java/com/example/board_project/domain/Board/dto/request/PostPatchRequest.java
+++ src/main/java/com/example/board_project/domain/Board/dto/request/PostPatchRequest.java
@@ -1,7 +1,9 @@
 package com.example.board_project.domain.Board.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
 public record PostPatchRequest(
         @Size(min = 1, max = 50, message = "제목은 1자에서 30자 사이로 입력해주세요.")
         String title,
