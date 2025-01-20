f3b297f00fcede7a4aee1c84df03286114ab19c4
YurimYang <yang980329@likelion.org>
2025-01-20 10:48:42 +0900

    fix: test를 위한 builder 어노테이션 추가

--- src/main/java/com/example/board_project/domain/Comment/dto/request/CommentPatchRequest.java
+++ src/main/java/com/example/board_project/domain/Comment/dto/request/CommentPatchRequest.java
@@ -1,7 +1,9 @@
 package com.example.board_project.domain.Comment.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
 public record CommentPatchRequest(
         @Size(min = 1, message = "댓글은 1자 이상 입력해주세요.")
         String content
